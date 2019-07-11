6c2dd843f5d478e162d491b956d59a4dd6a5c87d
kyleschaeffer <kyle@spirito.us>
2019-07-11 10:58:54 -0400

    Update constructor and add method to accept multiple values

--- src/avl-tree.test.ts
+++ src/avl-tree.test.ts
@@ -1,42 +1,20 @@
 import { Tree } from './avl-tree';
 
-test('tree is instantiated', () => {
-  const tree = new Tree<number>();
-  expect(tree.root).toBe(null);
-});
-
 test('values are added to tree', () => {
-  const tree = new Tree<number>();
-  tree.add(10);
-  tree.add(5);
+  const tree = new Tree<number>(10, 5);
   tree.add(15);
+  tree.add(17, 4, 6);
   expect(tree.root.value).toBe(10);
   expect(tree.root.left.value).toBe(5);
   expect(tree.root.right.value).toBe(15);
 });
 
 test('values are balanced in tree', () => {
-  const tree = new Tree<number>();
-  tree.add(1);
-  tree.add(2);
-  tree.add(3);
-  tree.add(4);
-  tree.add(5);
-  tree.add(6);
-  tree.add(7);
-  tree.add(8);
+  const tree = new Tree<number>(1, 2, 3, 4, 5, 6, 7, 8);
   expect(tree.root.value).toBe(4);
 });
 
 test('string values are balanced in tree', () => {
-  const tree = new Tree<string>();
-  tree.add('a');
-  tree.add('b');
-  tree.add('c');
-  tree.add('d');
-  tree.add('e');
-  tree.add('f');
-  tree.add('g');
-  tree.add('h');
+  const tree = new Tree<string>('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h');
   expect(tree.root.value).toBe('d');
 });
--- src/avl-tree.ts
+++ src/avl-tree.ts
@@ -4,7 +4,7 @@ import { Node } from './avl-tree-node';
  * AVL binary search tree data structure
  *  - Very fast lookup speeds
  *  - Self-balancing using AVL "rotation" technique
- *  - Worst case is O(log n) // better than basic BST
+ *  - Worst case is O(log n) –– better than basic BST
  */
 export class Tree<T> {
   /**
@@ -15,22 +15,27 @@ export class Tree<T> {
   /**
    * Instiantiate a new AVL tree
    */
-  constructor() {
+  constructor(...values: T[]) {
     // New empty tree
     this.root = null;
+
+    // Add values to the tree
+    this.add(...values);
   }
 
   /**
    * Add a value to the tree
    */
-  public add(value: T): void {
-    // New root
-    if (!this.root) {
-      this.root = new Node<T>(value);
-      return;
-    }
+  public add(...values: T[]): void {
+    values.forEach(value => {
+      // New root
+      if (!this.root) {
+        this.root = new Node<T>(value);
+        return;
+      }
 
-    // Add to tree
-    this.root.add(value);
+      // Add to tree
+      this.root.add(value);
+    });
   }
 }
